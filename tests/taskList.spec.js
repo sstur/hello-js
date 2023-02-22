1a0394411b2cc9b4737324c1256d11f190d297f4
Simon <sstur@me.com>
2023-02-22 12:50:12 -0800

    Setup task 57

--- tests/taskList.spec.js
+++ tests/taskList.spec.js
@@ -17,3 +17,17 @@ test('has task items', async ({ page }) => {
     '⌛️ Learn JavaScript',
   ]);
 });
+
+test('add a new task item', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  const textInput = page.getByPlaceholder('Enter a task');
+
+  await textInput.fill('Buy socks');
+
+  const button = page.getByRole('button', { name: 'Add Task' });
+
+  await button.click();
+
+  // TODO: Write an assertion that the task list now contains our new task
+});
