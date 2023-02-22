b4ef6d169fa523913ec0d9ea96f6c2d9ff15c0c0
Simon <sstur@me.com>
2023-02-22 10:45:13 -0800

    Begin tests for taskList

---
+++ tests/taskList.spec.js
@@ -0,0 +1,7 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await expect(page).toHaveTitle('Hello World');
+});
