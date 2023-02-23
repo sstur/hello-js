be37114498a6c590013f8daa6804811e5c4352f6
Simon <sstur@me.com>
2023-02-23 12:29:56 -0800

    Update URL for demo app

--- tests/homeFeed.spec.js
+++ tests/homeFeed.spec.js
@@ -2,7 +2,7 @@
 import { test, expect } from '@playwright/test';
 
 test('home feed loads', async ({ page }) => {
-  await page.goto('https://chirper.live-app.com/');
+  await page.goto('https://90u6ms-3000.preview.csb.app/');
 
   const headerTitle = page.getByText('Home', { exact: true });
 
@@ -12,7 +12,7 @@ test('home feed loads', async ({ page }) => {
 test('clicking create post (plus button) will navigate to login', async ({
   page,
 }) => {
-  await page.goto('https://chirper.live-app.com/');
+  await page.goto('https://90u6ms-3000.preview.csb.app/');
 
   // TODO: Locate the button, click it, assert that we have navigated to the /login page
 });
